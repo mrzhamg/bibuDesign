d8529318d9220587d0375648b6bd4b1a5ee4c9ce
kingkiss <kingkiss4love@gmail.com>
2022-01-10 19:43:55 +0800

    引入vxe-table，栏目管理完成部分

--- src/main.js
+++ src/main.js
@@ -4,12 +4,16 @@ import router from './router'
 import store from './store'
 import installElementPlus from './plugins/element'
 import './assets/css/icon.css'
+import 'xe-utils'
+import VXETable from 'vxe-table'
+import 'vxe-table/lib/style.css'
+
 
 
 const app = createApp(App)
 installElementPlus(app)
-app
-    .use(store)
-    .use(router)
-    .mount('#app')
-
+app.use(store)
+app.use(router)
+app.use(VXETable)
+app.mount('#app')
+    
